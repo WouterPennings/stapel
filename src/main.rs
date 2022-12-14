dbff04c7f86ecc8421b416ff720693f5f5fb0555
WouterPennings <wouterpennings@gmail.com>
2022-12-14 23:51:52 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ fn main() {
 
     // Checking and the arguments
     if args.len() == 1 && args[0] == "help" {
-        println!("USAGE:\n\tstapel  sim|com  <path>");
+        println!("USAGE:\n\tstapel com  <path>");
         std::process::exit(0);
     } else if args.len() != 2 {
         println!("Please provide two arguments\nType: 'stapel --help' for help");
         std::process::exit(0);
-    } else if args[0] != "sim" && args[0] != "com"  {
+    } else if  args[0] != "com"  {
         println!("'{}', is not a execution option.\nType: 'stapel --help' for help", args[1]);
     }
 
@@ -29,58 +29,53 @@ fn main() {
     let input = read_file(args[1].to_string());
     let program = parse(input);
     
-    // Running the Stapel code
-    if args[0] == "com" {
-        let mut compiler = Compiler::new(program);
-        compiler.compile_x86_64();
+    let mut compiler = Compiler::new(program);
+    compiler.compile_x86_64();
 
-        // Compiling stapel
-        let output = compiler.code;
-        println!("[INFO] Succesfully compiled Stapel to X86_64 assembly");
+    // Compiling stapel
+    let output = compiler.code;
+    println!("[INFO] Succesfully compiled Stapel to X86_64 assembly");
 
-        // Defining paths for compilation files
-        let assembly_path = format!("temp_{}.asm", args[1]);
-        let object_path = format!("temp_{}.asm.o", args[1]);
-        let executable_path = (&args[1][..(args[1].len()-4)]).to_string();
+    // Defining paths for compilation files
+    let assembly_path = format!("temp_{}.asm", args[1]);
+    let object_path = format!("temp_{}.asm.o", args[1]);
+    let executable_path = (&args[1][..(args[1].len()-4)]).to_string();
 
-        // Writing assembly file to fs
-        let res = std::fs::write(&assembly_path, output);
-        if let Err(_) = res {
-            println!("Could not save file at: {}", assembly_path);
-            std::process::exit(1);
-        }
+    // Writing assembly file to fs
+    let res = std::fs::write(&assembly_path, output);
+    if let Err(_) = res {
+        println!("Could not save file at: {}", assembly_path);
+        std::process::exit(1);
+    }
 
-        // Compiling ASM to object with nasm
-        let mut c = Command::new("sh");
-        c.arg("-c").arg(format!("nasm -f elf64 -o {} {}", object_path, assembly_path));
-        let output = c.output();
-        
-        // Printing result from nasm
-        if let Err(_) = output {
-            println!("[ERROR] Failed to compile NASM to *.o");
-            std::process::exit(1);
-        }
-        println!("[INFO] Sucessfully compiled NASM to object");
+    // Compiling ASM to object with nasm
+    let mut c = Command::new("sh");
+    c.arg("-c").arg(format!("nasm -f elf64 -o {} {}", object_path, assembly_path));
+    let output = c.output();
+    
+    // Printing result from nasm
+    if let Err(_) = output {
+        println!("[ERROR] Failed to compile NASM to *.o");
+        std::process::exit(1);
+    }
+    println!("[INFO] Sucessfully compiled NASM to object");
 
-        // Linking object
-        let mut c = Command::new("sh");
-        c.arg("-c").arg(format!("ld -o {} {}", executable_path, object_path));
-        let res = c.output();
+    // Linking object
+    let mut c = Command::new("sh");
+    c.arg("-c").arg(format!("ld -o {} {}", executable_path, object_path));
+    let res = c.output();
 
-        // Printing linking result
-        if let Err(_) = res {
-            println!("[ERROR] Failed to compile object file to binary");
-        } else {
-            println!("[INFO] Compilation succesfull, path to exe: './{}'", executable_path);
-        }
-
-        // Removing object file from compilation
-        let res = std::fs::remove_file(object_path);
-        if let Err(_) = res {
-            println!("[ERROR] Failed to remove object file");
-        }
+    // Printing linking result
+    if let Err(_) = res {
+        println!("[ERROR] Failed to compile object file to binary");
     } else {
-        run(program);
+        println!("[INFO] Compilation succesfull, path to exe: './{}'", executable_path);
+    }
+
+    // Removing object file from compilation
+    let res = std::fs::remove_file(object_path);
+    if let Err(_) = res {
+        println!("[ERROR] Failed to remove object file");
     }
 }
 
